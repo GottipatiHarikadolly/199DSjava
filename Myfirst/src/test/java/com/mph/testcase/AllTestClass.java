ecb3527712dda2348f9eaabbe316388284539b9d
GottipatiHarikaDolly <harika.gottipati333@gmail.com>
2021-10-23 17:09:11 +0530

    commit a Myfirst

---
+++ Myfirst/src/test/java/com/mph/testcase/AllTestClass.java
@@ -0,0 +1,9 @@
+package com.mph.testcase;
+
+import org.junit.runner.RunWith;
+
+@RunWith(suite.class)
+@suite.classes({CalculateTest.class,TestClass.class,UserTest.class})
+public class AllTestClass {
+
+}
